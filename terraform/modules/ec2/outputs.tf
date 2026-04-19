8b3c302351ef44c1386ed67fd0825c464412dc7d
AyushKat2001 <ayushkatiyar925@gmail.com>
2026-04-19 15:23:05 +0530

    Revised code for ansible playbook

--- terraform/modules/ec2/outputs.tf
+++ terraform/modules/ec2/outputs.tf
@@ -8,4 +8,4 @@ output "instance_id" {
     value = aws_instance.app.id
     description = "ID of the EC2 instance"
   
-}
+}  # Output the public IP address and instance ID of the EC2 instance created in this module for use in other modules or for informational purposes.
